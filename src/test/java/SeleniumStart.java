465c0fc3dd3308bb194cdb8fe05caa5f987f5681
Anna <hannagirl93@gmail.com>
2023-02-21 20:53:19 +0200

    changes

--- src/test/java/SeleniumStart.java
+++ src/test/java/SeleniumStart.java
@@ -1,8 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class SeleniumStart {
 
@@ -23,17 +27,22 @@ public class SeleniumStart {
     public void preCondition(){
         System.out.println("Opening web-site");
         wd = new ChromeDriver();
-        wd.get("https://telranedu.web.app/home");
+        //wd.get("https://telranedu.web.app/home");
+        wd.navigate().to("https://telranedu.web.app/home");
+//      wd.navigate().forward();
+//      wd.navigate().back();
     }
 
     @Test
     public void testLogin(){
-
+        WebElement element = wd.findElement(By.tagName("a"));
+        List<WebElement> elements = wd.findElements(By.tagName("a"));
     }
 
 
     @AfterMethod
     public void postCondition(){
-
+//      wd.close();
+        wd.quit();
     }
 }
